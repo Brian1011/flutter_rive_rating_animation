b7e3668a215aec8f55fcc6fbbb8a3b0814f9a2ea
Brian Mutinda <mutindabrian26@gmail.com>
2023-01-06 15:18:17 +0300

    initiate animation state

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,27 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late RiveAnimationController _btnAnimationController;
+
   animate() {}
+
+  initState() {
+    _btnAnimationController = OneShotAnimation(
+      "1_star",
+      autoplay: true,
+    );
+    super.initState();
+  }
+
+  /*
+    states that exist in the rive file
+    1_star
+    2_stars
+    3_stars
+    4_stars
+    5_stars
+  */
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +40,10 @@ class _HomeScreenState extends State<HomeScreen> {
             width: MediaQuery.of(context).size.width * 0.8,
             child: Column(
               children: [
-                const Expanded(
+                Expanded(
                   flex: 8,
-                  child: RiveAnimation.asset("assets/star_rating.riv"),
+                  child: RiveAnimation.asset("assets/star_rating.riv",
+                      controllers: [_btnAnimationController]),
                 ),
                 const SizedBox(height: 20),
                 TextButton(onPressed: animate, child: const Text("Animate"))
